1d583e948ae06366aab98df323508c775ace6549
Mihael <isaev.mihael@gmail.com>
2012-01-13 13:47:54 +0400

    Change template index.php

--- www/templates/index.php
+++ www/templates/index.php
@@ -1,11 +1,12 @@
 <!doctype html>
 <html>
     <head>
-        <title>Office</title>
+        <title>Web Office</title>
         <meta http-equiv="X-UA-Compatible"content="IE=EmulateIE7">
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <link type="text/css" href="/content/css/new.css" rel="stylesheet">
+        <link type="text/css" href="/content/css/style.css" rel="stylesheet">
         <script src="/content/scripts/jquery-1.6.2.min.js"></script>        
+        <script src="/content/scripts/helper.js"></script>
         <script src="/content/scripts/script.js"></script>
     </head>
     <body>
@@ -14,26 +15,10 @@
                 <div class="info"></div>
             </div>
         </div>
-        <div class="BrowserAlert">
-        	<div class="data">
-        		<div class="info">
-        			Вы используете неподходящий браузер<br>
-        			Установите Google Chrome<br>
-        			Скачать его можно по <a href="http://www.google.com/chrome/?hl=ru">здесь</a>
-        		</div>
-        	</div>
+        <div class="modalBackground"></div>
+        <div class="progressBar">
+            <div class="progressImage"></div>
         </div>
-        <div class="ModalBackground"></div>
-        <div class="ProgressBar">
-            <div class="ProgressImage"></div>
-            <div class="ProgressCancel"></div>
-        </div>
-        <div class="left">
-            <div class="data"></div>
-        </div>
-        <div class="right">
-            <div class="title"></div>
-            <div class="data"></div>
-        </div>
+        <div class="mainData"></div>
     </body>
 </html>
