9365786a3c2c0858be1dfe328f025f3ded2bc2df
Mihael <isaev.mihael@gmail.com>
2012-01-18 19:52:47 +0400

    Change language/ru.php: add new constants

--- www/content/language/ru.php
+++ www/content/language/ru.php
@@ -1,5 +1,11 @@
 <?
-$L[button][Enter] = 'Войти';
-$L[button][Forgot] = 'Забыли пароль?';
-$L[label][Email] = 'Эл.почта';
+$L[button][Enter]   = 'Войти';
+$L[button][Forgot]  = 'Забыли пароль?';
+$L[label][Email]    = 'Эл.почта';
 $L[label][Password] = 'Пароль';
+$L[label][Logout]   = 'Выход';
+$L[label][loginState][emptyLogin]     = "заполните поле";
+$L[label][loginState][wrongLogin]     = "пользователь не найден";
+$L[label][loginState][emptyPassword]  = "введите пароль";
+$L[label][loginState][lengthPassword] = "пароль не может быть меньше 6 символов";
+$L[label][loginState][wrongPassword]  = "неверный пароль";
