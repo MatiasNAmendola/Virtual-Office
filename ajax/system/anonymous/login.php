8c7d3c100927761fe4b9fd4aceaaa798908cb5ea
Mihael <isaev.mihael@gmail.com>
2012-01-18 19:49:27 +0400

    Change ajax/anonymous/login.php: use language file constants

--- ajax/system/anonymous/login.php
+++ ajax/system/anonymous/login.php
@@ -1,33 +1,33 @@
 <?
 
 $authorization = new authorization();
-$authState = $authorization->login($_POST['login'], $_POST['password']);
+$authState = $authorization->login($_POST['login'], $_POST['password'], $authorization::LOGIN_BY_EMAIL);
 
 switch($authState){
   case $authorization::STATE_LOGIN_EMPTY_LOGIN:
     echo json_encode(array('status'   => 'error',
-                            'login'    => 'Empty login',
+                            'login'    => $L[label][loginState][emptyLogin],
                             'password' => ''));
     break;
   case $authorization::STATE_LOGIN_WRONG_LOGIN:
     echo json_encode(array('status'   => 'error',
-                            'login'    => 'Wrong user',
+                            'login'    => $L[label][loginState][wrongLogin],
                             'password' => ''));
     break;
   case $authorization::STATE_LOGIN_EMPTY_PASSWORD:
     echo json_encode(array('status'   => 'error',
                             'login'    => '',
-                            'password' => 'Empty password'));
+                            'password' => $L[label][loginState][emptyPassword]));
     break;
   case $authorization::STATE_LOGIN_SHORT_PASSWORD:
     echo json_encode(array('status'   => 'error',
                             'login'    => '',
-                            'password' => 'Length of pasword < 6 characters'));
+                            'password' => $L[label][loginState][lengthPassword]));
     break;
   case $authorization::STATE_LOGIN_WRONG_PASSWORD:
     echo json_encode(array('status'   => 'error',
                             'login'    => '',
-                            'password' => 'Wrong password'));
+                            'password' => $L[label][loginState][wrongPassword]));
     break;
   case $authorization::STATE_LOGIN_SUCCESS:
     echo json_encode(array('status' => 'success'));
