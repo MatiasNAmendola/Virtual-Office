9ea2593426de82240b1d9202fa04bdc51f728be3
Mihael <isaev.mihael@gmail.com>
2012-01-18 19:53:45 +0400

    Change templates/index.php: add scripts to head

--- www/templates/index.php
+++ www/templates/index.php
@@ -5,9 +5,13 @@
         <meta http-equiv="X-UA-Compatible"content="IE=EmulateIE7">
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <link type="text/css" href="/content/css/style.css" rel="stylesheet">
-        <script src="/content/scripts/jquery-1.6.2.min.js"></script>        
+        <script src="/content/scripts/jquery-1.6.2.min.js"></script>
         <script src="/content/scripts/helper.js"></script>
-        <script src="/content/scripts/script.js"></script>
+        <script src="/content/scripts/visual.js"></script>
+        <script src="/content/scripts/ajax.js"></script>
+        <script src="/content/scripts/binder.js"></script>
+        <script src="/content/scripts/auth.js"></script>
+        <script src="/content/scripts/system.js"></script>
     </head>
     <body>
         <div class="ModalWindow">
