5ddf3e7d541eaa93436e9eaaf7d9084db90983f4
Mihael <isaev.mihael@gmail.com>
2012-01-18 19:55:44 +0400

    Add new files to html/

---
+++ ajax/html/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Staff list
+ */
+?>
+<div class="title">Настройки</div>
+<div class="left">
+    <ul>
+        <li class="active">Компания</li>
+        <li>Личные</li>
+    </ul>
+</div>
+<div class="right">
+    Здесь будут настройки :)
+</div>
---
+++ ajax/html/tasks.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Staff list
+ */
+?>
+<div class="title">Задачи</div>
+<div class="left">
+    <ul>
+        <li class="active">Выполненные</li>
+        <li>В процессе</li>
+        <li>Невыполненные</li>
+        <li>Добавить задачу</li>
+    </ul>
+</div>
+<div class="right">
+    Здесь будет список задач
+</div>
