7654f29b6d46aae5cce9abae95002601dfa63161
Mihael <isaev.mihael@gmail.com>
2012-01-18 19:51:50 +0400

    Change classes/dbSchema.php: add new public variables

--- classes/dbSchema.php
+++ classes/dbSchema.php
@@ -9,7 +9,15 @@
 class dbSchema
 {
     public $TABLE_USERS = 'staff';
-    
     public $CELL_USERS_LOGIN    = 'Login';
+    public $CELL_USERS_EMAIL    = 'Email';
     public $CELL_USERS_PASSWORD = 'Password';
+    
+    public $TABLE_DEPARTMENTS = 'departments';
+    public $CELL_DEPARTMENTS_IDORGANIZATION    = 'IdOrganization';
+    public $CELL_DEPARTMENTS_NAME              = 'Name';
+    
+    public $TABLE_POSTS = 'posts';
+    public $CELL_POSTS_IDORGANIZATION    = 'IdOrganization';
+    public $CELL_POSTS_NAME              = 'Name';
 }
